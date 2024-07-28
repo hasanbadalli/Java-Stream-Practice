33d4d3fbfc258fb7224f9ade6efef14441b91314
hasanbadalli <hasanoaga2005@gmail.com>
2024-07-28 16:23:45 +0400

    task 24

--- src/tasks/task24.java
+++ src/tasks/task24.java
@@ -1,4 +1,26 @@
 package tasks;
 
+import Model.Employee;
+import datas.UserData;
+import enums.Department;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class task24 {
+    public static void main(String[] args) {
+        //Find the oldest employee in each department.
+        Map<Department, Optional<Employee>> oldestEmployeeInEachDept = UserData.userList.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::getDepartment,
+                        Collectors.maxBy(Comparator.comparingInt(Employee::getAge))
+                ));
+
+        oldestEmployeeInEachDept.forEach((department, employeeOpt) -> {
+            employeeOpt.ifPresent(employee ->
+                    System.out.println(department + " - Oldest Employee: " + employee.getName() + " " + employee.getSurname() + ", Age: " + employee.getAge()));
+        });
+    }
 }
